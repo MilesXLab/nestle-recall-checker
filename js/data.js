d9e31f5483565640bc1d2749b9c8b171268e6b0c
MilesXLab <miles.x.labs@gmail.com>
2026-01-25 18:40:43 -0800

    revert: UI reverted to main branch as requested; data preserved (375 batches)

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@
 
 const RECALL_METADATA = {
     "version": "4.5.2 (100% Official)",
-    "lastUpdated": "2026-01-24 05:38 (PST)",
+    "lastUpdated": "2026-01-25 18:40 (PST)",
     "coverage": "9 Regions - Official Government Sources Only",
     "totalCount": 375,
     "authority": "FSA, CFS, FDA, RappelConso, FSANZ, SAMR",
